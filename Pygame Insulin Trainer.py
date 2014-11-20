e97822b6e2adba724bdf70c2f90f504abeafb5ae
Sofia Chandler-Freed <sofiachandlerfreed@gmail.com>
2014-11-20 16:54:59 -0700

    Fixing some stuff
    Now currentNumber works!
    Also, entering non-ints won't break everything!
    Now with... fewer global variables! Hooray!
    Things are slightly more in comprehendible order now, too.

--- Pygame Insulin Trainer.py
+++ Pygame Insulin Trainer.py
@@ -4,16 +4,15 @@ pygame.init()
 
 def Setup():
 	global width, height, screen, key, windowBlitted, graph
-	global zero, one, two, three, four, five, six, seven, eight, nine, numbers, currentNumber, inputNumbers
+	global zero, one, two, three, four, five, six, seven, eight, nine, numbers
+	global inputNumbers, firstNumber, secondNumber, thirdNumber
 	(width, height) = (500, 500)
 	screen = pygame.display.set_mode((width, height))
 	key = pygame.key.get_pressed()
 	pygame.display.set_caption("This is the python version of Insulin Trainer")
 	submit = False
 	graph = pygame.Surface((width-100, height-100))
-	inputNumbers = []
-	currentNumber = ""
-	font = pygame.font.Font(None, 14)
+
 	class Number():
 		def __init__(self, name, image, value):
 			self.name = name
@@ -30,7 +29,15 @@ def Setup():
 	eight = Number(pygame.K_8, pygame.image.load("8.png"), 8)
 	nine = Number(pygame.K_9, pygame.image.load("9.png"), 9)
 	numbers = [zero, one, two, three, four, five, six, seven, eight, nine]
+
+	currentNumber = ""
+	inputNumbers = []
+	firstNumber = [None, (0, 0)]
+	secondNumber = [None, (10, 0)]
+	thirdNumber = [None, (20, 0)]
+
 Setup()
+
 class Button():
 	def __init__(self, image, isSelected, commonName, yOffset):
 		self.image = image
@@ -70,49 +77,6 @@ allTheButtons = [bloodSugarInputButton, foodConsumptionButton, insulinDoseButton
 def displayMenuButtons():
 	for button in MenuButtons:
 		screen.blit(button.image, button.pos)
-
-firstNumber = [None, (0, 0)]
-secondNumber = [None, (10, 0)]
-thirdNumber = [None, (20, 0)]
-def displayInputWindow():
-	for button in MenuButtons:
-		if button.selected == True and cancel.selected == False:
-			windowBlitted = True
-			screen.blit(button.field, button.fieldpos)
-			screen.blit(cancel.image, cancel.pos)
-			screen.blit(add.image, add.pos)
-			return True
-		elif cancel.selected == True:
-			return False
-def collectInput(event):
-	global firstNumber, secondNumber, thirdNumber, inputNumbers, currentNumber
-	if displayInputWindow():
-			if event.type == pygame.KEYDOWN:
-				if firstNumber[0] == None:
-					firstNumber[0] = event.unicode
-					inputNumbers.append(firstNumber)
-				elif secondNumber[0] == None:
-					secondNumber[0] = event.unicode
-					inputNumbers.append(secondNumber)
-				elif thirdNumber[0] == None:
-					thirdNumber[0] = event.unicode
-					inputNumbers.append(thirdNumber)
-def submit():
-	global currentNumber
-	for inputnumber in inputNumbers:
-		if inputnumber[0] and len(currentNumber) <= 2:
-			currentNumber = currentNumber + inputnumber[0]
-	if len(currentNumber) >= 1:
-		int(currentNumber)
-def displayInput(): 
-	if displayInputWindow():  
-		for number in numbers:
-			if str(number.value) == firstNumber[0]:
-				screen.blit(number.image, firstNumber[1])
-			if str(number.value) == secondNumber[0]:
-				screen.blit(number.image, secondNumber[1])
-			if str(number.value) == thirdNumber[0]:
-				screen.blit(number.image, thirdNumber[1])
 def isButtonPressed():
 	if pygame.mouse.get_pressed()[0] == True:
 		for button in allTheButtons:
@@ -125,6 +89,50 @@ def isButtonPressed():
 			if inX and inY:
 				button.selected = True
 			else: button.selected = False
+def displayInputWindow():
+	for button in MenuButtons:
+		if button.selected == True and cancel.selected == False:
+			windowBlitted = True
+			screen.blit(button.field, button.fieldpos)
+			screen.blit(cancel.image, cancel.pos)
+			screen.blit(add.image, add.pos)
+			return True
+		elif cancel.selected == True:
+			return False
+def collectInput(event):
+	global firstNumber, secondNumber, thirdNumber, inputNumbers
+	if displayInputWindow():
+		if event.type == pygame.KEYDOWN:
+			if event.unicode in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+				if firstNumber[0] == None:
+					print(event.unicode)
+					firstNumber[0] = event.unicode
+					inputNumbers.append(firstNumber)
+				elif secondNumber[0] == None:
+					secondNumber[0] = event.unicode
+					inputNumbers.append(secondNumber)
+				elif thirdNumber[0] == None:
+					thirdNumber[0] = event.unicode
+					inputNumbers.append(thirdNumber)
+			else:
+				print("That is not acceptable input. Please try again.")
+def submit():
+	currentNumber = ""
+	if len(currentNumber) < len(inputNumbers):
+		for inputnumber in inputNumbers:
+				currentNumber = currentNumber + inputnumber[0]
+	if len(currentNumber) >= 1:
+		currentNumber = int(currentNumber)
+	return currentNumber
+def displayInput(): 
+	if displayInputWindow():  
+		for number in numbers:
+			if str(number.value) == firstNumber[0]:
+				screen.blit(number.image, firstNumber[1])
+			if str(number.value) == secondNumber[0]:
+				screen.blit(number.image, secondNumber[1])
+			if str(number.value) == thirdNumber[0]:
+				screen.blit(number.image, thirdNumber[1])
 
 sampleImage = pygame.image.load("Sample Image.jpg")
 clock = pygame.time.Clock()
